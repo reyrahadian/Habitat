aeaad63891f5e05019afa6c797303a8ecca679f8
Sean Holmesby <sholmesby@gmail.com>
2016-11-23 13:02:00 -0500

    More updates of 'interfaces' to 'contracts'.

--- src/Feature/Demo/contracts/Properties/AssemblyInfo.cs
+++ src/Feature/Demo/contracts/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Sitecore.Feature.Demo.Interfaces")]
+[assembly: AssemblyTitle("Sitecore.Feature.Demo.Contracts")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Sitecore.Feature.Demo.Interfaces")]
+[assembly: AssemblyProduct("Sitecore.Feature.Demo.Contracts")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
