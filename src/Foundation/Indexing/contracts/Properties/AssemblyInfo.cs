0346f6535b5f15fe95ce58cd0f150ae037d816fe
Sean Holmesby <sholmesby@gmail.com>
2016-11-23 11:17:12 -0500

    Made ZD config for Indexing contracts match existing setup.

--- src/Foundation/Indexing/contracts/Properties/AssemblyInfo.cs
+++ src/Foundation/Indexing/contracts/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("contracts")]
+[assembly: AssemblyTitle("Sitecore.Foundation.Indexing.Contracts")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("contracts")]
+[assembly: AssemblyProduct("Sitecore.Foundation.Indexing.Contracts")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -33,7 +33,11 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
+#if ZERODEPLOY
 [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-
 [assembly: ZeroDeployAssembly]
+#else
+[assembly: AssemblyVersion("1.0.0.0")]
+#endif
+
+[assembly: AssemblyFileVersion("1.0.0.0")]
