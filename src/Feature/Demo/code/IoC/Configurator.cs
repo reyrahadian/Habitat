aaa4264892e8b1fd8b3888cb88b3c27ad1572ae0
Sean Holmesby <sholmesby@gmail.com>
2016-12-08 11:12:44 -0500

    Converted Feature.Demo's IoC over to a configurator instead of config.

---
+++ src/Feature/Demo/code/IoC/Configurator.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Sitecore.DependencyInjection;
+using Sitecore.Feature.Demo.Contracts.Services;
+using Sitecore.Feature.Demo.Services;
+using Sitecore.Foundation.Accounts.Providers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Feature.Demo.IoC
+{
+    public class Configurator : IServicesConfigurator
+    {
+        public void Configure(IServiceCollection serviceCollection)
+        {
+            serviceCollection
+                .AddTransient<IProfileProvider, ProfileProvider>()
+                .AddTransient<IContactProfileProvider, ContactProfileProvider>();
+
+#if ZERODEPLOY == false
+            serviceCollection
+                .AddTransient<Controllers.DemoController>();
+#endif
+        }
+    }
+}
